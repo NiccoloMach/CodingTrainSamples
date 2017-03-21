e8676c8628b7510a9c2fb2a5815d47215050fc0d
NiccoloMach <markprojects888@gmail.com>
2017-03-22 00:14:40 +0800

    changed filename

---
+++ 2.2-circle/sketch.js
@@ -0,0 +1,17 @@
+var circleX = 250;
+
+function setup() {
+    createCanvas(600, 400);   
+}
+
+function draw() {
+    // background
+    background(250, 250, 100);
+    // ellipse
+    fill(250, 200, 200);
+    ellipse(circleX, 200, 80, 80);
+    
+    circleX = circleX + 1;
+
+}
+ 
